5a0f83132e312a13b8124ddc7349f14fca6457db
Emanuel Geromin <emanuel.geromin@onfido.com>
2024-12-09 12:49:00 +0000

    Moons example - incomplete

--- micrograd/training/core.py
+++ micrograd/training/core.py
@@ -1,13 +1,20 @@
+from typing import Iterable
+
 from engine import Value
 from nn import MLP
 
 
 def train_with_sgd(
     mlp: MLP,
-    features: list[list[float | int]],
-    labels: list[float | int],
+    features: Iterable[Iterable[float | int]],
+    labels: Iterable[float | int],
     loss_threshold: float = 1e-3,
+    max_steps: int | None = None,
 ) -> tuple[MLP, float, list[float]]:
+    # Collect the features and labels
+    features = [list(f) for f in features]
+    labels = list(labels)
+
     def mse(outputs: list[Value]) -> Value:
         assert len(outputs) == len(labels)
         sum_of_squares = sum(
@@ -21,9 +28,11 @@ def train_with_sgd(
         loss := mse(predictions := [mlp(inp)[0] for inp in inputs])
     ).data > loss_threshold:
         step += 1
-        if step % 100 == 0:
-            print(f"loss at step {step}: {loss.data}")
-        learning_rate = 1e-1 if loss.data > 1e-2 else 1e-2
+        if max_steps and step > max_steps:
+            print(f"Ending at {max_steps=}")
+            break
+        print(f"loss at step {step}: {loss.data}")
+        learning_rate = 0.5 if loss.data > 0.4 else 0.1 if loss.data > 0.09 else 1e-2
         # Backprop and update parameters.
         loss.backward()
         for param in mlp.parameters():
---
+++ micrograd/training/moons.py
@@ -0,0 +1,51 @@
+import numpy as np
+from engine import Value
+from sklearn.datasets import make_moons
+from matplotlib import pyplot as plt
+
+from training.core import train_with_sgd
+from nn import MLP
+
+
+def main():
+    features, labels = make_moons(n_samples=100, noise=0.1)
+
+    labels = labels * 2 - 1  # make y be -1 or 1
+    # visualize in 2D
+    # plt.figure(figsize=(5, 5))
+    # plt.scatter(features[:, 0], features[:, 1], c=labels, s=20, cmap='jet')
+    # plt.show()
+
+    mlp = MLP(2, [2, 3, 2], 1)
+    mlp, final_loss, final_predictions = train_with_sgd(
+        mlp, features, labels, max_steps=20
+    )
+    print(f"Final loss: {final_loss}")
+    print(f"Final predictions: {final_predictions}")
+
+    # How to visualise the decision boundary?
+    # Use a grid
+    padding_factor = 0.05
+    x_min, x_max = features[:, 0].min(), features[:, 0].max()
+    x_padding = (x_max - x_min) * padding_factor
+    y_min, y_max = features[:, 1].min(), features[:, 1].max()
+    y_padding = (y_max - y_min) * padding_factor
+    xx, yy = np.meshgrid(
+        np.linspace(-x_min - x_padding, x_max + x_padding, 20),
+        np.linspace(-y_min - y_padding, y_max + y_padding, 20),
+    )
+
+    predictions = []
+    for x, y in zip(xx.ravel(), yy.ravel()):
+        output = mlp([Value(x), Value(y)])[0]
+        predictions.append(output.data)
+
+    predictions_np = np.array(predictions).reshape(xx.shape)
+    plt.figure(figsize=(5, 5))
+    plt.contourf(xx, yy, predictions_np, alpha=0.3, cmap="jet")
+    plt.scatter(features[:, 0], features[:, 1], c=labels, s=20, cmap=plt.cm.coolwarm)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
