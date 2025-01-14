f83034e33b3eda49d1adfadb57af49a48317e008
Emanuel Geromin <emanuel.geromin@onfido.com>
2025-01-14 12:46:03 +0100

    Implement training loop, overfit on a batch

--- makemore/mlp.py
+++ makemore/mlp.py
@@ -8,6 +8,7 @@ import itertools
 from pathlib import Path
 
 import torch
+import torch.nn.functional as F
 
 
 def load_dataset(context_size: int):
@@ -47,19 +48,26 @@ def load_dataset(context_size: int):
 
 class MLP:
     def __init__(
-        self, vocab_size: int, context_size: int, embedding_size: int, hidden_size: int
+        self,
+        vocab_size: int,
+        context_size: int,
+        embedding_size: int,
+        hidden_size: int,
+        g: torch.Generator,
     ):
         self.embedding: torch.Tensor = torch.randn(
-            size=(vocab_size, embedding_size), requires_grad=True
+            size=(vocab_size, embedding_size), requires_grad=True, generator=g
         )
         self.hidden_w: torch.Tensor = torch.randn(
-            size=(embedding_size * context_size, hidden_size), requires_grad=True
+            size=(embedding_size * context_size, hidden_size),
+            requires_grad=True,
+            generator=g,
         )
         self.hidden_b: torch.Tensor = torch.zeros(
             size=(hidden_size,), requires_grad=True, dtype=torch.float
         )
         self.output_w: torch.Tensor = torch.randn(
-            size=(hidden_size, vocab_size), requires_grad=True
+            size=(hidden_size, vocab_size), requires_grad=True, generator=g
         )
         self.output_b: torch.Tensor = torch.zeros(
             size=(vocab_size,), requires_grad=True, dtype=torch.float
@@ -76,9 +84,50 @@ class MLP:
         output = hidden_nonlin @ self.output_w + self.output_b
         return output
 
+    def zero_grad(self):
+        self.embedding.grad = None
+        self.hidden_w.grad = None
+        self.hidden_b.grad = None
+        self.output_w.grad = None
+        self.output_b.grad = None
 
-def train_model(mlp: MLP, X: torch.Tensor, Y: torch.Tensor) -> MLP:
-    pass
+    def update_parameters(self, learning_rate: float):
+        self.embedding.data -= learning_rate * self.embedding.grad
+        self.hidden_w.data -= learning_rate * self.hidden_w.grad
+        self.hidden_b.data -= learning_rate * self.hidden_b.grad
+        self.output_w.data -= learning_rate * self.output_w.grad
+        self.output_b.data -= learning_rate * self.output_b.grad
+
+
+def train_model(mlp: MLP, X: torch.Tensor, Y: torch.Tensor, g: torch.Generator) -> MLP:
+    # Grab a minibatch. V1: overfit on a minibatch. Later: different minibatch per iteration.
+    batch_size = 100
+    batch_idx = torch.randperm(len(X), generator=g)[:batch_size]
+    X_batch = X[batch_idx]
+    Y_batch = Y[batch_idx]
+    assert X_batch.shape == (batch_size, X.shape[1])
+    assert Y_batch.shape == (batch_size,)
+
+    num_training_iterations = 5000
+    reg_alpha = 0.01
+    learning_rate = 0.1
+    for i in range(num_training_iterations):
+        mlp.zero_grad()
+        logits_batch = mlp.forward(X_batch)
+        model_loss = F.cross_entropy(logits_batch, Y_batch) / batch_size
+        reg_loss = reg_alpha * ((mlp.hidden_w**2).sum() + (mlp.output_w**2).sum())
+
+        loss = model_loss + reg_loss
+
+        predictions = logits_batch.argmax(dim=-1)
+        assert predictions.shape == Y_batch.shape
+        accuracy = sum(predictions == Y_batch) / batch_size
+        print(f"Step {i}: loss = {loss.item():.4f}, accuracy = {accuracy * 100:.2f}%")
+
+        loss.backward()
+        mlp.update_parameters(learning_rate)
+
+    return mlp
 
 
 def sample_from_model(mlp: MLP):
@@ -88,14 +137,20 @@ def sample_from_model(mlp: MLP):
 def main():
     context_size = 3
     X, Y, stoi = load_dataset(context_size=context_size)
+
+    g = torch.Generator().manual_seed(2147483647)
     mlp = MLP(
         vocab_size=len(stoi),
         context_size=context_size,
         embedding_size=11,
         hidden_size=13,
+        g=g,
     )
     output = mlp.forward(X[:20, :])
     print(output.shape)
+
+    mlp = train_model(mlp, X, Y, g)
+    sample_from_model(mlp)
 
 
 if __name__ == "__main__":
