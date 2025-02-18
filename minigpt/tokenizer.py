9c86ad8b969b115a86a2369a77e634793823e067
Emanuel Geromin <emanuel.geromin@onfido.com>
2025-02-18 19:36:51 +0000

    Implement handling of the GPT-2 regex

--- minigpt/tokenizer.py
+++ minigpt/tokenizer.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
 from pathlib import Path
 
+import regex
 
 # TODOs:
 # Simplify the implementation to use `merges` and `vocab` dictionary.  ✅
 # Fix decoding invalid tokens using the "replace" mechanism  ✅
-# Implement the GPT-2 regex for splitting and tokenizing each chunk separately.
+# Implement the GPT-2 regex for splitting and tokenizing each chunk separately. ✅
 # Play around with tiktoken - specifically, reproduce the SolidGoldMagikarp issue
 # Play around with sentencepiece and use it to tokenize
 # Try implementing adding a new token to an existing mini-GPT model and then finetuning it.
@@ -18,13 +19,21 @@ class Tokenizer:
         self.vocab_size = 256
         self.merges = []
         self.vocab = {i: bytes([i]) for i in range(self.vocab_size)}
-        tokens = [int(b) for b in input_text.encode("utf-8")]
+        self.r50k_pat_str = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}++| ?\p{N}++| ?[^\s\p{L}\p{N}]++|\s++$|\s+(?!\S)|\s"""
+        self.r50k_pat = regex.compile(self.r50k_pat_str)
+        text_chunks = self.r50k_pat.findall(input_text)
+        tokens_per_chunk = [
+            [int(b) for b in chunk.encode("utf-8")] for chunk in text_chunks
+        ]
         while self.vocab_size < target_vocab_size:
-            top_tuple = self.find_most_common_pair(tokens)
+            top_tuple = self.find_most_common_pair(tokens_per_chunk)
             if top_tuple is None:
                 print("No more tokens to merge, aborting prematurely")
                 break
-            tokens = self._substitute(tokens, list(top_tuple), [self.vocab_size])
+            tokens_per_chunk = [
+                self._substitute(chunk, list(top_tuple), [self.vocab_size])
+                for chunk in tokens_per_chunk
+            ]
             self.merges.append((self.vocab_size, top_tuple))
             self.vocab_size += 1
 
@@ -35,18 +44,24 @@ class Tokenizer:
             )
 
     @classmethod
-    def _get_pair_counts(cls, tokens: list[int]) -> dict[tuple[int, int], int]:
-        pair_counts = defaultdict(int)
+    def _get_pair_counts(
+        cls,
+        tokens: list[int],
+        existing_pair_counts: dict[tuple[int, int], int] | None = None,
+    ) -> dict[tuple[int, int], int]:
+        pair_counts = existing_pair_counts or defaultdict(int)
         for a, b in zip(tokens, tokens[1:]):
             pair_counts[(a, b)] += 1
         return pair_counts
 
     @classmethod
-    def find_most_common_pair(cls, tokens: list[int]) -> tuple[int, int] | None:
+    def find_most_common_pair(
+        cls, tokens_per_chunk: list[list[int]]
+    ) -> tuple[int, int] | None:
         """Find the most common pair of tokens"""
-        if len(tokens) < 2:
-            return None
-        pair_counts = cls._get_pair_counts(tokens)
+        pair_counts = defaultdict(int)
+        for chunk in tokens_per_chunk:
+            pair_counts = cls._get_pair_counts(chunk, existing_pair_counts=pair_counts)
         tuples_sorted = sorted(pair_counts.items(), key=lambda x: x[1], reverse=True)
         top_tuple = next(iter(tuples_sorted))[0]
         return top_tuple
@@ -69,10 +84,17 @@ class Tokenizer:
                 i += 1
         return output_tokens
 
-    def encode(self, text: str) -> list[int]:
-        tokens = [int(b) for b in text.encode("utf-8")]
+    def encode_chunk(self, text_chunk: str) -> list[int]:
+        tokens = [int(b) for b in text_chunk.encode("utf-8")]
         for token, original_pair in self.merges:
             tokens = self._substitute(tokens, list(original_pair), [token])
+        return tokens
+
+    def encode(self, text: str) -> list[int]:
+        text_chunks = self.r50k_pat.findall(text)
+        tokens = []
+        for chunk in text_chunks:
+            tokens.extend(self.encode_chunk(chunk))
         return tokens
 
     def decode(self, tokens: list[int]) -> str:
