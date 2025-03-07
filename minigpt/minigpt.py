Fix tok/s calculation

minigpt/minigpt.py
@@ -430,7 +430,7 @@ def train(
             num_accum_steps += 1
             with torch.autocast(device_type=device, dtype=torch.bfloat16):
                 logits = model.forward(X_batch)
-                # Calculate the loss for each of the tokens in the input
+                # Calculate the loss for each of the tokens in the input)
                 loss = F.cross_entropy(
                     logits.view(-1, model.config.vocab_size),
                     Y_batch.view(-1),
@@ -450,7 +450,7 @@ def train(
         torch.cuda.synchronize()
         end = time.time()
         elapsed = end - start
-        n_tok = X_batch.shape[0] * X_batch.shape[1]
+        n_tok = X_batch.shape[0] * X_batch.shape[1] * grad_accum_steps
         tok_ps = n_tok / elapsed
 
         train_loss_estimate = sum(train_losses[-20:]) / len(train_losses[-20:])