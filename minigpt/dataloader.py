996eeec7287b5f926c032576d2bf0c64eb85cf57
Emanuel Geromin <emanuel.geromin@onfido.com>
2025-03-11 18:03:36 +0000

    Fix dataloader bugs and tests

--- minigpt/dataloader.py
+++ minigpt/dataloader.py
@@ -12,7 +12,6 @@ class TokenizerInterface(Protocol):
 
 
 class DataLoader:
-
     def __init__(
         self,
         batch_size: int,
@@ -20,7 +19,9 @@ class DataLoader:
         tokens: list[int] | None = None,
         ddp_rank: int = 0,
         ddp_world_size: int = 1,
-        files: list[Path] = None,
+        files: list[Path] | None = None,
+        max_tokens_to_load: int | None = None,
+        files_dtype: np.dtype = np.uint16,
     ):
         if tokens is None and files is None:
             raise ValueError("One of `tokens` or `files` must be provided.")
@@ -34,8 +35,15 @@ class DataLoader:
         self.files = files
         self.current_file_idx = 0
         self.current_file = None
-        self.max_tokens_to_load = 1e6
+        self.max_tokens_to_load = max_tokens_to_load or int(1e6)
         self.current_global_pos = 0
+        self.files_dtype = files_dtype
+
+        assert self.max_tokens_to_load >= self.n_tokens_in_batch, (
+            "Need to load more tokens than number of tokens in batch."
+        )
+
+        self.refresh_tokens()
 
     @property
     def n_tokens_in_batch(self) -> int:
@@ -63,28 +71,39 @@ class DataLoader:
         return len(self.tokens) // (self.batch_size * self.context_size)
 
     def refresh_tokens(self):
-        tokens = self.tokens[self.current_global_pos:]
+        tokens = self.tokens[self.current_global_pos :]
         if self.files is None:
-            tokens += self.tokens[:self.current_global_pos]
+            tokens += self.tokens[: self.current_global_pos]
         else:
             while len(tokens) < self.max_tokens_to_load:
                 if self.current_file is None:
                     self.current_file = self.files[self.current_file_idx].open("rb")
-                new = np.fromfile(self.current_file, dtype=np.int16, count=self.max_tokens_to_load)
+                new = list(
+                    np.fromfile(
+                        self.current_file,
+                        dtype=self.files_dtype,
+                        count=self.max_tokens_to_load,
+                    )
+                )
                 tokens += new
                 if len(new) == 0:
                     self.current_file.close()
                     self.current_file = None
-                    self.current_file_idx = (self.current_file_idx + 1) % len(self.files)
+                    self.current_file_idx = (self.current_file_idx + 1) % len(
+                        self.files
+                    )
 
         self.tokens = tokens
         self.current_global_pos = 0
 
     def __iter__(self) -> Iterator[tuple[torch.tensor, torch.tensor]]:
-
         while True:
-            inputs = self.tokens[self.current_pos : self.current_pos + self.n_tokens_in_batch]
-            labels = self.tokens[self.current_pos + 1 : self.current_pos + self.n_tokens_in_batch + 1]
+            inputs = self.tokens[
+                self.current_pos : self.current_pos + self.n_tokens_in_batch
+            ]
+            labels = self.tokens[
+                self.current_pos + 1 : self.current_pos + self.n_tokens_in_batch + 1
+            ]
             inputs_tensor = torch.tensor(inputs, dtype=torch.long).view(
                 self.batch_size, self.context_size
             )
@@ -168,24 +187,38 @@ def main():
 
 def test_from_files():
     dataloader = DataLoader(
-        files=[
-            Path(f"test-dataloader/batch_{i}.npy")
-            for i in range(10)
-        ],
+        files=[Path(f"test-dataloader/batch_{i}.npy") for i in range(10)],
         batch_size=16,
-        context_size=1024
+        context_size=1024,
+        max_tokens_to_load=459812,
+        files_dtype=np.uint32,
     )
     train = []
     labels = []
     max_to_fetch = 1000000
+    dataloader_it = iter(dataloader)
     while len(train) < max_to_fetch:
-        for train_t, labels_t in dataloader:
-            train += train_t.view(-1).tolist()
-            labels += labels_t.view(-1).tolist()
+        train_t, labels_t = next(dataloader_it)
+        train += train_t.view(-1).tolist()
+        labels += labels_t.view(-1).tolist()
     train = train[:max_to_fetch]
     labels = labels[:max_to_fetch]
+
+    # Print the first diff
+    for i in range(len(train)):
+        if train[i] != i:
+            print(f"{i}th position of 'train' is {train[i]}")
+            break
+
     assert train == list(range(max_to_fetch))
-    assert labels == list(range(1, max_to_fetch+1))
+
+    # Print the first diff
+    for i in range(len(labels)):
+        if labels[i] != i + 1:
+            print(f"{i}th position of 'labels' is {labels[i]}")
+            break
+
+    assert labels == list(range(1, max_to_fetch)) + [0]
 
     # TODO: test with different values of world_size and ddp_rank
 
